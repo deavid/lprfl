86428cebd5c07cde6f5eb53212e10b32fe8d31d9
0lhi <74732674+0lhi@users.noreply.github.com>
2022-08-09 21:06:21 +0200

    Changed variable names to improve readability

--- projects/survivalpong/src/main.rs
+++ projects/survivalpong/src/main.rs
@@ -36,23 +36,23 @@ async fn main_loop() {
     set_sound_volume(over, 2.0);
 
     let mut score = 0;
-    // BALL
-    let mut x_pos: f32 = screen_width() - 50.0;
-    let mut y_pos = screen_height() / 2.0;
-    let mut dx = -5.0;
-    let mut dy = 4.0;
+    // BALL POSITION
+    let mut ball_pos_x: f32 = screen_width() - 50.0;
+    let mut ball_pos_y = screen_height() / 2.0;
+    let mut ball_direction_x = -5.0;
+    let mut ball_direction_y = 4.0;
     let speed_mult = 1.1;
-    // PADDLE
-    let paddle_x: f32 = 30.0;
-    let mut paddle_y: f32 = screen_height() / 2.0;
+    // PADDLE POSITION
+    let paddle_pos_x: f32 = 30.0;
+    let mut paddle_pos_y: f32 = screen_height() / 2.0;
     // BALL SIZES
-    let size = 10.0;
-    let size_2 = size / 2.0;
+    let ball_size = 10.0;
+    let ball_size_apothem = ball_size / 2.0;
     // PADDLE SIZES
-    let sizex = 10.0;
-    let mut sizey = 150.0; // shortens over time.
-    let sizex_2 = sizex / 2.0;
-    let mut sizey_2 = sizey / 2.0;
+    let paddle_size_x = 10.0;
+    let mut paddle_size_y = 150.0; // shortens over time.
+    let paddle_size_x_apothem = paddle_size_x / 2.0;
+    let mut paddle_size_y_apothem = paddle_size_y / 2.0;
 
     loop {
         let now = Instant::now();
@@ -72,91 +72,125 @@ async fn main_loop() {
 
         let prec_steps = 20;
         for i in 0..prec_steps {
-            x_pos += dx / prec_steps as f32;
-            y_pos += dy / prec_steps as f32;
+            ball_pos_x += ball_direction_x / prec_steps as f32;
+            ball_pos_y += ball_direction_y / prec_steps as f32;
 
             // DOES BALL HIT WALL?
-            if y_pos - size_2 < scr_margin {
-                y_pos = scr_margin + size_2;
-                dy = dy.abs();
+            if ball_pos_y - ball_size_apothem < scr_margin {
+                ball_pos_y = scr_margin + ball_size_apothem;
+                ball_direction_y = ball_direction_y.abs();
                 score += 1;
                 play_sound_once(click);
             }
-            if y_pos + size_2 > screen_height() - scr_margin {
-                y_pos = screen_height() - scr_margin - size_2;
-                dy = -dy.abs();
+            if ball_pos_y + ball_size_apothem > screen_height() - scr_margin {
+                ball_pos_y = screen_height() - scr_margin - ball_size_apothem;
+                ball_direction_y = -ball_direction_y.abs();
                 score += 1;
                 play_sound_once(click);
             }
-            if x_pos + size_2 < 0.0 {
+            if ball_pos_x + ball_size_apothem < 0.0 {
                 // FOUL! Init the velocities and position
-                dx = -5.0;
-                dy = 4.0;
-                x_pos = screen_width() - 50.0;
-                y_pos = screen_height() / 2.0;
+                ball_direction_x = -5.0;
+                ball_direction_y = 4.0;
+                ball_pos_x = screen_width() - 50.0;
+                ball_pos_y = screen_height() / 2.0;
                 score /= 2;
-                // sizey = 150.0;
-                // sizey_2 = sizey / 2.0;
+                // paddle_size_y = 150.0;
+                // paddle_size_y_apothem = paddle_size_y / 2.0;
                 play_sound_once(over);
             }
-            if x_pos + size_2 > screen_width() - scr_margin {
-                x_pos = screen_width() - scr_margin - size_2;
-                dx = -dx.abs();
+            if ball_pos_x + ball_size_apothem > screen_width() - scr_margin {
+                ball_pos_x = screen_width() - scr_margin - ball_size_apothem;
+                ball_direction_x = -ball_direction_x.abs();
                 score += 1;
                 play_sound_once(click);
             }
 
             // DOES BALL HIT PADDLE?
-            if x_pos - size_2 < paddle_x + sizex_2 && x_pos + size_2 > paddle_x - sizex_2 {
+            if ball_pos_x - ball_size_apothem < paddle_pos_x + paddle_size_x_apothem
+                && ball_pos_x + ball_size_apothem > paddle_pos_x - paddle_size_x_apothem
+            {
                 // In the same X.
-                if y_pos - size_2 < paddle_y + sizey_2 && y_pos + size_2 > paddle_y - sizey_2 {
+                if ball_pos_y - ball_size_apothem < paddle_pos_y + paddle_size_y_apothem
+                    && ball_pos_y + ball_size_apothem > paddle_pos_y - paddle_size_y_apothem
+                {
                     // In the same Y
-                    if dx < 0.0 {
-                        dx = dx.abs();
-                        let speed = f32::sqrt(dx * dx + dy * dy) * speed_mult;
-                        let diff_y = (y_pos - paddle_y) / sizey_2;
-                        dy += diff_y * 5.0;
-                        let speed2 = f32::sqrt(dx * dx + dy * dy);
+                    if ball_direction_x < 0.0 {
+                        ball_direction_x = ball_direction_x.abs();
+                        let speed = f32::sqrt(
+                            ball_direction_x * ball_direction_x
+                                + ball_direction_y * ball_direction_y,
+                        ) * speed_mult;
+                        let diff_y = (ball_pos_y - paddle_pos_y) / paddle_size_y_apothem;
+                        ball_direction_y += diff_y * 5.0;
+                        let speed2 = f32::sqrt(
+                            ball_direction_x * ball_direction_x
+                                + ball_direction_y * ball_direction_y,
+                        );
                         let f = (speed / speed2).sqrt();
-                        dx *= f;
-                        dy *= f;
+                        ball_direction_x *= f;
+                        ball_direction_y *= f;
                         score += 1;
-                        sizey /= 1.05;
-                        sizey_2 = sizey / 2.0;
+                        paddle_size_y /= 1.05;
+                        paddle_size_y_apothem = paddle_size_y / 2.0;
                         play_sound_once(snare);
                     }
                 }
             }
             let f = i as f32 / prec_steps as f32;
             let trail_color = Color::new(0.00, 0.50 * f, 1.00 * f, 1.00);
-            draw_rectangle(x_pos - size_2, y_pos - size_2, size, size, trail_color);
+            draw_rectangle(
+                ball_pos_x - ball_size_apothem,
+                ball_pos_y - ball_size_apothem,
+                ball_size,
+                ball_size,
+                trail_color,
+            );
         }
         // BALL RENDERING
         let ball_color = Color::new(1.00, 0.75, 0.50, 0.50);
-        draw_rectangle(x_pos - size_2, y_pos - size_2, size, size, ball_color);
+        draw_rectangle(
+            ball_pos_x - ball_size_apothem,
+            ball_pos_y - ball_size_apothem,
+            ball_size,
+            ball_size,
+            ball_color,
+        );
 
         // KEYBOARD INPUT - TO PADDLE
-        draw_rectangle(paddle_x - sizex_2, paddle_y - sizey_2, sizex, sizey, BROWN);
+        draw_rectangle(
+            paddle_pos_x - paddle_size_x_apothem,
+            paddle_pos_y - paddle_size_y_apothem,
+            paddle_size_x,
+            paddle_size_y,
+            BROWN,
+        );
         let up = is_key_down(KeyCode::W);
         let down = is_key_down(KeyCode::S);
         if up {
-            paddle_y -= 10.0;
+            paddle_pos_y -= 10.0;
         }
         if down {
-            paddle_y += 10.0;
+            paddle_pos_y += 10.0;
         }
-        if paddle_y - sizey_2 < scr_margin {
-            paddle_y = scr_margin + sizey_2;
+        if paddle_pos_y - paddle_size_y_apothem < scr_margin {
+            paddle_pos_y = scr_margin + paddle_size_y_apothem;
         }
-        if paddle_y + sizey_2 > screen_height() - scr_margin {
-            paddle_y = screen_height() - scr_margin - sizey_2;
+        if paddle_pos_y + paddle_size_y_apothem > screen_height() - scr_margin {
+            paddle_pos_y = screen_height() - scr_margin - paddle_size_y_apothem;
         }
-        if sizey < 150.0 {
-            sizey += 0.03;
-            sizey_2 = sizey / 2.0;
+        if paddle_size_y < 150.0 {
+            paddle_size_y += 0.03;
+            paddle_size_y_apothem = paddle_size_y / 2.0;
         }
         // PADDLE RENDERING
-        draw_rectangle(paddle_x - sizex_2, paddle_y - sizey_2, sizex, sizey, YELLOW);
+        draw_rectangle(
+            paddle_pos_x - paddle_size_x_apothem,
+            paddle_pos_y - paddle_size_y_apothem,
+            paddle_size_x,
+            paddle_size_y,
+            YELLOW,
+        );
 
         // UI - FPS METERS
         let fps = format!("{:.2}fps", 1.0 / get_frame_time());
